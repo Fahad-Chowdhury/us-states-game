daf663abb8a1b6256ddee76bef53f79e88dbd57a
Fahad <fahad@mail.fi>
2024-11-02 00:53:58 +0200

    Simplify read us states

--- read_us_states.py
+++ read_us_states.py
@@ -14,7 +14,6 @@ class CsvHandler:
     def read_states_coordinates(self):
         """ Read the states coordinates from DATA_FILE and returns it as a Python
         dictionary. """
-        self.states_coord = {}
         current_dir = os.path.dirname(os.path.abspath(__file__))
         filepath = os.path.join(current_dir, DATA_FILE)
         data = pandas.read_csv(filepath)
@@ -25,14 +24,8 @@ class CsvHandler:
 
     def generate_state_to_learn(self, guessed_states):
         """ Generate a CSV file with the states list that the user failed to guess. """
-        missed_states = []
-        for state in self.states_coord:
-            if state not in guessed_states:
-                missed_states.append(state)
-        states = {
-            "Missing States": missed_states,
-        }
-
+        missed_states = [state for state in self.states_coord if state not in guessed_states]
+        states = {"Missing States": missed_states,}
         data_frame = pandas.DataFrame(states)
         current_dir = os.path.dirname(os.path.abspath(__file__))
         filepath = os.path.join(current_dir, STATES_TO_LEARN_FILE)
